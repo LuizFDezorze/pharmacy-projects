7d716f3679d27f44408cccbcb538131bb092f1f6
Luiz Fernando Dezorze <lfernando.dezorze@gmail.com>
2023-02-10 17:33:53 -0300

    views and msg created. Testing register a new user

--- home_app/views.py
+++ home_app/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
 
 def home(request):
     return render(request, "home_app/home.html")
 
+# form de cadastro de users
 def create(request):
     return render(request, "home_app/create.html")
 
+# add data into the db
 def store(request):
-    pass
+    data = {}
+    if (request.POST['password'] != request.POST['password-conf']):
+        data['msg'] = 'Senhas diferentes!'
+        data['class'] = 'alert-danger'
+    else:
+        user = User.objects.create_user(
+            request.POST['user'],
+            request.POST['email'],
+            request.POST['password']
+        )
+        user.first_name = request.POST['name']
+        user.save()
+        data['msg'] = 'Usuário cadastrado com sucesso!'
+        data['class'] = 'alert-success'
+    return render(request, 'home_app/create.html', data)
