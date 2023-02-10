7471ab920e38e73a26964a51ea432d47f2b32cb3
Luiz Fernando Dezorze <lfernando.dezorze@gmail.com>
2023-02-10 18:15:55 -0300

    login created

--- home_app/urls.py
+++ home_app/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('create/', views.create, name="create"),
     path('store/', views.store, name="store"),
+    path('loginpage/', views.loginpage, name="loginpage"),
+    path('dologin/', views.dologin, name="dologin"),
+    path('dashboard/', views.dashboard, name="dashboard"),
 ]
--- home_app/views.py
+++ home_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 def home(request):
     return render(request, "home_app/home.html")
@@ -25,3 +26,24 @@ def store(request):
         data['msg'] = 'Usuário cadastrado com sucesso!'
         data['class'] = 'alert-success'
     return render(request, 'home_app/create.html', data)
+
+# pag de login
+def loginpage(request):
+    return render(request, 'home_app/login_page.html')
+
+# processa o login
+def dologin(request):
+    data = {}
+    user = authenticate(username=request.POST['user'], password=request.POST['password'])
+    if user is not None:
+        login(request, user)
+        return redirect('/dashboard/')
+    else: 
+        data['msg'] = 'Usuário ou senha inválidos!'
+        data['class'] = 'alert-danger'
+        return render(request, 'home_app/login_page.html', data)
+
+
+# area privada
+def dashboard(request):
+    return render(request, 'home_app/dashboard.html')
